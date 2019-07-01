69dc315792a4fa7eb8caf90212375b7f5763c9fd
LucyGwilliamAdmin <43404358+LucyGwilliamAdmin@users.noreply.github.com>
2019-07-01 14:49:39 +0100

    Update deploy_prod.sh

--- scripts/deploy/circleci/deploy_prod.sh
+++ scripts/deploy/circleci/deploy_prod.sh
@@ -6,11 +6,11 @@
 # This is the Github organisation or username of your fork. For example, if
 # your fork is located at https://github.com/xyz/open-sdg-site-starter, then
 # you should put "xyz" here. Changing this is required.
-GH_ORG_PROD="sustainabledevelopment-kyrgyzstan"
+GH_ORG_PROD="SDG-Kyrgyzstan"
 # These variables only control the name/email on the automated Git commits.
 # Changing this is optional.
-GH_NAME="CircleCI"
-GH_EMAIL="no-reply@example.com"
+GH_NAME="LucyGwilliamAdmin"
+GH_EMAIL="lucy.gwilliam@ons.gov.uk"
 ###################################
 
 # There is probably no need to modify anything below this point.
@@ -20,10 +20,10 @@ git config --global user.name "$GH_NAME"
 # CircleCI will identify the SSH key with a "Host" of gh-prod. In order to tell
 # Git to use this key, we need to hack the SSH key:
 sed -i -e 's/Host gh-prod/Host gh-prod\n  HostName github.com/g' ~/.ssh/config
-git clone git@gh-prod:$GH_ORG_PROD/$CIRCLE_PROJECT_REPONAME.git out
+git clone git@gh-prod:$GH_ORG_PROD/sdg-kazstat.github.io.git out
 
 cd out
-git checkout gh-pages || git checkout --orphan gh-pages
+git checkout master || git checkout --orphan master
 git rm -rfq .
 cd ..
 
@@ -33,7 +33,10 @@ cp -a ~/repo/_site/. out/.
 mkdir -p out/.circleci && cp -a .circleci/. out/.circleci/.
 cd out
 
+# Create the CNAME file which Github Pages needs for custom domains.
+#echo "sdg.stat.kz" > CNAME
+
 git add -A
 git commit -m "Automated deployment to GitHub Pages: ${CIRCLE_SHA1}" --allow-empty
 
-git push origin gh-pages
+git push origin master
