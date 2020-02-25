46c50f26c27cfd61e64b6e6d140ce96b65f6d8dc
Lucy <43404358+LucyGwilliamAdmin@users.noreply.github.com>
2020-02-25 19:49:18 +0000

    Update custom.js

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,14 +1,13 @@
 opensdg.dataRounding = function(value) {
-  var value = value.toString()
   if (value == null) {
     return value
   }
   else {
-    if (value.includes(".")) {
-      if (value.split(".")[1].length == 1) {
+    if (value.toString().includes(".")) {
+      if (value.toString().split(".")[1].length == 1) {
         return value.toFixed(1)
       }
-      else if (value.split(".")[1].length >= 2) {
+      else if (value.toString().split(".")[1].length >= 2) {
         return value.toFixed(2)
       }
     }
