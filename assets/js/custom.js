47dbdc5b0171e2cd1e8dcd8fffd5ad83cc341508
LucyGwilliamAdmin <lucy.gwilliam@ons.gov.uk>
2020-02-25 19:20:17 +0000

    Automated deployment to GitHub Pages: 45a35a50f475fa08f80fcf0942a9eec89450f2fc

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -6,13 +6,15 @@ opensdg.dataRounding = function(value) {
   else {
     if (value.includes(".")) {
       if (value.split(".")[1].length == 1) {
-          return value.toFixed(1)
+        return value.toFixed(1)
       }
       else if (value.split(".")[1].length >= 2) {
         return value.toFixed(2)
       }
+    }
     else {
       return value
-    }
+    }    
   }
 };
+
