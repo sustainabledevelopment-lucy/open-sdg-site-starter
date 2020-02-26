d1a6e3ecb431cdae9b80472931490b85cba287db
LucyGwilliamAdmin <LucyGwilliamAdmin@users.noreply.github.com>
2020-02-26 09:13:23 +0000

    Deploying to gh-pages from  - 303256a8bbf58710ad2ee05091b162bc482233c8 🚀

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -3,17 +3,7 @@ opensdg.dataRounding = function(value) {
     return value
   }
   else {
-    if (value.toString().includes(".")) {
-      if (value.toString().split(".")[1].length == 1) {
-        return value.toFixed(1)
-      }
-      else if (value.toString().split(".")[1].length >= 2) {
-        return value.toFixed(2)
-      }
-    }
-    else {
-      return value
-    }    
+    return value.toFixed(2)
   }
 };
 
